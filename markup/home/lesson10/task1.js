4a969dd48b5d916ac4f79183c59a1323c82382fe
Akim <ulfnordson@gmail.com>
2015-05-13 15:33:08 +0300

    js upd complete

--- markup/home/lesson10/task1.js
+++ markup/home/lesson10/task1.js
@@ -1,21 +1,19 @@
 "use strict";
 $(document).ready(function () {
-
-    var clone,
-        thumb;
+    var item_number,
+        clone;
 
     $(".js-gallery-zoom").click(function () {
-        $(".gallery__blackout").css("opacity", "0.1");
-
-        alert(thumb);
-        $(".galllery__preview-box").show();
-
-        clone = $(this).find(".gallery__image").clone();
+        item_number = $(this).parents(".gallery__item").index();
+        clone = $("[data-number='" + item_number + "']");
+        $("[data-number='" + item_number + "']").removeClass("gallery__image").addClass("gallery__image-preview");
         $(".galllery__preview-box").append(clone);
-
-
+        $(".galllery__preview-box").show();
+         $(".gallery__container").hide();
     });
 
-
+    $(".gallery__x-button").click(function () {
+        location.reload();
+    });
 
 });
