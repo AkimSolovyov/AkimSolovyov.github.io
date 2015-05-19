d9b070d84a1f38a18f7a217cc057d7c86690995a
AkimSolovyov <ulfnordson@gmail.com>
2015-05-19 17:58:49 +0300

    desktop markup upd

--- markup/project2/js/scripts.js
+++ markup/project2/js/scripts.js
@@ -1,3 +1,5 @@
 $('#header-form').submit(function () {
+    /* draft variant for select submit */
+    // $('#header-form_select-content').val($('#header-form_select').html());
     $('#header-form_adress-content').val($('#header-form_adress').html());
 });
