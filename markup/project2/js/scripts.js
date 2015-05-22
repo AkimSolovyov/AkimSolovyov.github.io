6bd3c50ce1f9ef287f8788c1ea7598cbe3411bc2
AkimSolovyov <ulfnordson@gmail.com>
2015-05-22 13:39:13 +0300

    project2 upd

--- markup/project2/js/scripts.js
+++ markup/project2/js/scripts.js
@@ -1,5 +1,12 @@
-$('#header-form').submit(function () {
-    /* draft variant for select submit */
-    // $('#header-form_select-content').val($('#header-form_select').html());
-    $('#header-form_adress-content').val($('#header-form_adress').html());
-});
+$(document).ready(function () {
+    $('#header-form').submit(function () {
+        /* draft variant for select submit */
+        // $('#header-form_select-content').val($('#header-form_select').html());
+        $('#header-form_adress-content').val($('#header-form_adress').html());
+    });
+
+    $('.header-form__input:not(#header-form_select)').click(function () {
+        document.execCommand('selectAll', false, null)
+
+    });
+})
