8d4d534c7d1716eb0cb5d4b2f1bde4340320b7ea
AkimSolovyov <ulfnordson@gmail.com>
2015-06-09 17:39:58 +0300

    adaptive layout fix

--- markup/testathon/js/scripts.js
+++ markup/testathon/js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     function setHeight() {
         windowHeight = $(window).innerHeight();
-        windowWidth = $(window).innerWidth();
         $('.for-tester').css('min-height', windowHeight - 425);
         $('.for-startup').css('min-height', windowHeight - 50);
         $('.for-startup').css('padding-top', windowHeight / 10);
@@ -17,87 +16,66 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         setHeight();
-        initGallery();
     });
 
+});
 
+function initGallery() {
+    windowWidth = $(window).innerWidth();
+    if (windowWidth > 620) {
+        $('.gallery-zoomed').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: false,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            cssEase: 'linear',
+            asNavFor: '.gallery'
+        });
 
-    function initGallery() {
+        $('.gallery').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.gallery-zoomed',
+            dots: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
 
-        if (windowWidth > 420) {
-            $('.gallery-zoomed').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                arrows: false,
-                fade: true,
-                autoplay: true,
-                autoplaySpeed: 4000,
-                cssEase: 'linear',
-                asNavFor: '.gallery'
-            });
+    } else {
+        $('.gallery-zoomed').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: false,
+            fade: true,
+            autoplay: false,
+            cssEase: 'linear',
+            asNavFor: '.gallery'
+        });
 
-            $('.gallery').slick({
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                asNavFor: '.gallery-zoomed',
-                dots: false,
-                centerMode: true,
-                focusOnSelect: true
-            });
-
-        } else {
-            $('.gallery-zoomed').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                arrows: false,
-                fade: true,
-                cssEase: 'linear',
-                asNavFor: '.gallery'
-            });
-
-            $('.gallery').slick({
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                asNavFor: '.gallery-zoomed',
-                dots: false,
-                centerMode: true,
-                focusOnSelect: true
-            });
-
-        }
-    };
-
-
-    function toggleMenu() {
-        $('.bottom-nav__btn').click(function () {
-            $('.bottom-nav').toggleClass('bottom-nav-expand')
-        })
+        $('.gallery').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            asNavFor: '.gallery-zoomed',
+            dots: false,
+            centerMode: true,
+            focusOnSelect: true
+        });
 
     }
 
-});
+
+};
 
 
-
-var menu = 1;
-var omenu = 1;
-
-$(window).scroll(function () {
-    curpos = $(document).scrollTop();
-    if (curpos < $('.s1').offset().top) {
-        menu = 1;
-    } else if ((curpos >= $('.s2').offset().top) && (curpos < $('.s3').offset().top)) {
-        menu = 2;
-    } else if ((curpos >= $('.s3').offset().top) && (curpos < $('.s4').offset().top)) {
-        menu = 3;
-    } else if (curpos >= $('.s4').offset().top) {
-        menu = 4;
-    } else if (curpos >= $('.s5').offset().top) {
-        menu = 5;
-    }
-});
+function toggleMenu() {
+    $('.bottom-nav__btn').click(function () {
+        $('.bottom-nav').toggleClass('bottom-nav-expand')
+    })
+}
 
 function show(id) {
     var h = $('.s' + id).offset().top;
