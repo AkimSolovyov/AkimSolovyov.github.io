a1261e124cc641c73fd7e2fb8487ea2c314771e7
AkimSolovyov <ulfnordson@gmail.com>
2015-06-18 17:19:07 +0300

    upd

--- markup/project3/js/scripts.js
+++ markup/project3/js/scripts.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
 
+    var checked_item;
+
+
+    $('.menu__menu-item').click(function () {
+        $('.menu__menu-item').removeClass('menu__menu-item-hovered');
+        $(this).addClass('menu__menu-item-hovered');
+        checked_item = $(this);
+    });
+
+    $('.menu-item__link').click(function () {
+        $('.menu-item__link').removeClass('menu-item__link-hovered');
+        $('.submenu-item__link').removeClass('submenu-item__link-hovered');
+        $(this).addClass('menu-item__link-hovered');
+    });
+
+    $('.submenu-item__link').click(function () {
+        $('.submenu-item__link').removeClass('submenu-item__link-hovered');
+        $(this).addClass('submenu-item__link-hovered');
+        $('.menu-item__link').removeClass('menu-item__link-hovered');
+        $(this).parent().parent().parent().find('.menu-item__link').addClass('menu-item__link-hovered');
+    });
+
+
+    $(".menu__menu-item").hover(
+        function () {
+            $('.menu__menu-item').removeClass('menu__menu-item-hovered');
+            $(this).addClass('menu__menu-item-hovered');
+        },
+        function () {
+            $('.menu__menu-item').removeClass('menu__menu-item-hovered');
+            checked_item.addClass('menu__menu-item-hovered');
+        }
+    );
+
+
 })
