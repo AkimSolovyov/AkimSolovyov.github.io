0a3f11ea2ae05d74c14608d36fd5d7533e2d8add
AkimSolovyov <ulfnordson@gmail.com>
2015-10-20 16:12:48 +0300

    upd

--- projects/mbs/js/scripts.js
+++ projects/mbs/js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     function setHeight() {
         windowHeight = $(window).innerHeight();
         $('.hero').css('min-height', windowHeight);
-        $('.hero .hero-content').css('padding-top', windowHeight / 6);
+        $('.hero .hero-content').css('padding-top', windowHeight / 4);
 
 
     };
@@ -15,17 +15,78 @@ $(document).ready(function () {
     });
 
 
+    function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+            center: new google.maps.LatLng(50.4474701, 30.4219109),
+            zoom: 18,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+        var companyPos = new google.maps.LatLng(50.447556, 30.422345);
+        var companyMarker = new google.maps.Marker({
+            position: companyPos,
+            map: map,
+            title: "Zeo Allience"
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    $(".speakers").owlCarousel({
+        // Most important owl features
+        items: 2,
+        itemsCustom: false,
+        itemsDesktop: [1200, 3],
+        itemsDesktopSmall: [750, 2],
+        itemsTablet: [550, 1],
+        itemsTabletSmall: true,
+        itemsMobile: [320, 1],
+        singleItem: false,
+        itemsScaleUp: false,
+
+        //Basic Speeds
+        slideSpeed: 200,
+        paginationSpeed: 800,
+        rewindSpeed: 1000,
+
+        //Autoplay
+        autoPlay: false,
+        stopOnHover: false,
+
+        // Navigation
+        navigation: true,
+        navigationText: ["", ""],
+        rewindNav: true,
+        scrollPerPage: false,
+
+        //Pagination
+        pagination: false,
+        paginationNumbers: false,
+
+        // Responsive
+        responsive: true,
+        responsiveRefreshRate: 200,
+        responsiveBaseWidth: window,
+
+        // CSS Styles
+        baseClass: "owl-carousel",
+        theme: "owl-theme",
+
+        //Lazy load
+        lazyLoad: false,
+        lazyFollow: true,
+        lazyEffect: "fade",
 
 
-    /***************** Initiate Flexslider ******************/
-    $('.flexslider').flexslider({
-        animation: "slide",
-        animationLoop: false,
-        itemWidth: 320,
-        itemMargin: 5,
-        minItems: 1,
-        maxItems: 2
+        //Mouse Events
+        dragBeforeAnimFinish: true,
+        mouseDrag: true,
+        touchDrag: true,
+
     });
+
 
 
     /***************** Nav Transformicon ******************/
@@ -62,9 +123,11 @@ $(document).ready(function () {
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
-                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "30px 0 65px 0"
+                    /*  "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                      "padding": "30px 0 25px 0"*/
                 });
+
+                $('header').addClass('.header-border');
 
                 $('header .navicon').css({
                     "top": "48px",
